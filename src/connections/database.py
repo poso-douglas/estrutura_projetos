c8cac7f23e68dfb7e0acb3a5d3cc0d15ea1a1db1
poso-douglas <douglas.poso@gmail.com>
2024-08-05 10:14:20 -0300

    Correção arquivo database.py

--- src/connections/database.py
+++ src/connections/database.py
@@ -12,8 +12,8 @@ dbport = os.getenv("POSTGRES_PORT")
 dbhost = os.getenv("POSTGRES_HOST")
 dbdatabase = os.getenv("POSTGRES_DATABASE")
 
-#CONNECTION_STRING = f"postgresql://{dbuser}:{dbpass}@{dbhost}:{dbport}/{dbdatabase}"
-CONNECTION_STRING = "postgresql://admin:dtrHmdkFOoDf7ummtCMx0LitzbbeR1qO@dpg-cqn7rfdsvqrc73fl0q8g-a.oregon-postgres.render.com:5432/sales_db_9zgl"
+CONNECTION_STRING = f"postgresql://{dbuser}:{dbpass}@{dbhost}:{dbport}/{dbdatabase}"
+
 engine = create_engine(CONNECTION_STRING, echo=True)
 #Garante que a seção do banco será fechada
 session = sessionmaker(bind=engine)
